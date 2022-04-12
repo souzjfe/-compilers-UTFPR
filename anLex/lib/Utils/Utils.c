2676199bc86bf274468b1680eef7e1cc34d9f053
Jeferson Souza <souzajefison@gmail.com>
2022-04-12 11:29:57 -0300

    💎 - style: add color on error message

--- anLex/lib/Utils/Utils.c
+++ anLex/lib/Utils/Utils.c
@@ -1,9 +1,16 @@
 #include <string.h>
 #include "Utils.h"
+#include "../OrderedList/OrderedList.h"
+#include "../Token/Token.h"
 int contCaracter(char *string, char caracter){
     int cont = 0;
     for(int i=0; string[i]!='\0'; i++)
         if(string[i]==caracter)
             cont++;
     return cont;
+}
+
+void insertOrderedListAndPrintToken(OrderedList *list, Token x) {
+  insertOrderedList(list,x);
+  printToken(x);
 }
--- anLex/lib/Utils/Utils.h
+++ anLex/lib/Utils/Utils.h
@@ -1,5 +1,7 @@
 
-
+#include "../OrderedList/OrderedList.h"
+#include "../Token/Token.h"
 
 
 int contCaracter(char *string, char caracter);
+void insertOrderedListAndPrintToken(OrderedList *list, Token x);
